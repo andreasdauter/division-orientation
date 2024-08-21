c0af4ab09aac6a0c6faaba152099e4def320e1ab
andreasdauter <andreas.dauter@ucalgary.ca>
2024-08-21 14:16:53 -0600

    Region plotting script now supports multiple plots in parallel
    To do: better display of multiple plots at once

--- Scripts/plotOrientationRegions.R
+++ Scripts/plotOrientationRegions.R
@@ -1,7 +1,10 @@
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(readr)
 library(circular)
+library(purrr)
+#library(tidyverse)
 
 #set directory path and load in data
 fullpath <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
@@ -11,22 +14,37 @@ all_samples <- list.files(path=filepath, pattern = "*.csv", full.names=TRUE)
 spindles = read.csv(all_samples[1])
 
 ###Subset sample by position along the vertical axis###
-#divisions = 5
-#total_rows = nrow(spindles)/divisions
-#region_rows = total_rows / divisions
-#spindle_regions = split(spindles, rep(1:ceiling(total_rows), each=region_rows, length.out=(total_rows)))
 
+# Split df by position along z axis
+spindle_z = split(spindles, spindles$FileName_Spindle)
 
+#Set number of desired divisions
+divisions = 6
+#Divide dataset into (mostly) equally sized groups of z slices
+total_z = length(spindle_z)
+region_z = total_z / divisions
+spindle_regions = split(spindle_z, rep(1:floor(total_z), each=region_z, length.out=(total_z)))
 
+#Specify colours to iterate through
+colour_list = c("red", "orange", "yellow", "green", "cyan", "blue")
 
-# Make orientation data circular in each subset
-spindleOrientationA = circular(spindles$AreaShape_Orientation,type = "angles", units = "degrees",zero = pi/2)
-spindleOrientationB = circular(spindles$AreaShape_Orientation + 180,type = "angles", units = "degrees",zero = pi/2)
-spindleOrientation = c(spindleOrientationA,spindleOrientationB)
+###Generate one rose plot per region###
+for(i in 1:length(spindle_regions)){
+  # Make orientation data circular in each subset
+  region_spindles = Reduce(full_join, Reduce(full_join, spindle_regions[i]))  
+  spindleOrientationA = circular(region_spindles$AreaShape_Orientation,type = "angles", units = "degrees",zero = pi/2)
+  spindleOrientationB = circular(region_spindles$AreaShape_Orientation + 180,type = "angles", units = "degrees",zero = pi/2)
+  spindleOrientation = c(spindleOrientationA,spindleOrientationB)
+  if (i==1) {
+  rose.diag(spindleOrientation, pch = 16, cex = 1, axes = TRUE, shrink = 1, bins = 24,
+            col = colour_list[i], border = "darkgray", radii.scale = "linear", prop = 14, tol = 0.02, tcl.text = 0.075, add=FALSE, alpha = 0.5)
+  } else {
+  rose.diag(spindleOrientation, pch = 16, cex = 1, axes = TRUE, shrink = 1, bins = 24,
+            col = colour_list[i], border = "darkgray", radii.scale = "linear", prop = 14, tol = 0.02, tcl.text = 0.075, add=TRUE, alpha = 0.5)    
+  }
+  # test for significance
+  print(i)
+  kuiper.test(x=spindleOrientation)
+}
+test = Reduce(full_join, Reduce(full_join, spindle_regions[1]))         
 
-rose.diag(spindleOrientation, pch = 16, cex = 1, axes = TRUE, shrink = 1, bins = 24,
-          col = "cadetblue3", border = "cadetblue", radii.scale = "linear", prop = 14, tol = 0.02, tcl.text = 0.075, add = TRUE)
-
-# test for significance
-kuiper.test(x=spindleOrientation)
-          
