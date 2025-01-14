e97fcd911b74be27d6e049b4465d22e334e37b1b
andreasdauter <andreas.dauter@ucalgary.ca>
2025-01-14 15:14:42 -0700

    Added file pathing

--- Scripts/pairObjectsCoordinates.R
+++ Scripts/pairObjectsCoordinates.R
@@ -14,10 +14,10 @@ pair_closest_points <- function(df1, df2) {
   
   # Iterate over each row in df1
   for (i in 1:nrow(df1)) {
-    point1 <- df1[i, c("x", "y", "z")]
+    point1 <- df1[i, c("Location_Center_X", "Location_Center_Y", "Location_Center_Z")]
     
     # Calculate distances to all points in df2
-    distances <- apply(df2[, c("x", "y", "z")], 1, function(row) euclidean_distance(point1, row))
+    distances <- apply(df2[, c("Location_Center_X", "Location_Center_Y", "Location_Center_Z")], 1, function(row) euclidean_distance(point1, row))
     
     # Find the row number of the closest point in df2
     closest_row <- which.min(distances)
@@ -30,12 +30,29 @@ pair_closest_points <- function(df1, df2) {
   return(paired_points)
 }
 
+#####
 # Load in both dataframes
+fullpath <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+filepath <- paste(fullpath,"Data", sep="/")
+all_samples <- list.files(path=filepath, pattern = "*.csv", full.names=TRUE)
+# Change names of files to load that contain coordinates.
+points1 = read.csv(paste(filepath,"CellGeom_SpindlePole.csv", sep="/"))
+points2 = read.csv(paste(filepath,"CellGeom_CellShape.csv", sep="/"))
 
-
-# Convert z-slice to z coordinate
+# Convert z-slices to z coordinates
 zscale_factor = 5 #ratio of z-slice thickness to xy pixel size
 
+#Fetch and multiply the Z-slice for each row
+for (i in 1:nrow(df1)) {
+test_string = ("AurA_June7_A_3_frontface_z155c1.tiff")
+z_stack = as.numeric(str_sub(test_string, -10, -8))
+# TO DO: pull and insert into the Location_Z column
+}
+
+# Or, piped:
+test_string %>% str_sub() %>%
+  as.numeric()
+  
 
 # Find closest pairs using row numbers
 paired_rows <- pair_closest_points(df1, df2)
@@ -44,5 +61,4 @@ paired_rows <- pair_closest_points(df1, df2)
 print(paired_rows)
 
 
-test_string = ("AurA_June7_A_3_frontface_z155c1.tiff")
-z_stack = as.numeric(str_sub(test_string, -10, -8))
+
